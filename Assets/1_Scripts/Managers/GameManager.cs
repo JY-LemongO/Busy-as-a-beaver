ff59e54786b3bc0a9a4d558e8d000a9b01492c45
LKM0222WIN <python03@naver.com>
2025-03-02 00:26:13 +0900

    저장되는 데이터 딕셔너리 저장방법 변경 // 데이터 접근방법은 GameManager 상단에 작성해뒀습니다!

--- Assets/1_Scripts/Managers/GameManager.cs
+++ Assets/1_Scripts/Managers/GameManager.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 /// <summary>
 /// 접근방법 : GameManager.Instance.변수명
 /// 필요한 데이터는 var에 정리해주세요!
+/// 
+/// 
+/// 저장되는 데이터(체력, 이동속도, 재화 등등)은 statusData에 저장됩니다!
+/// 접근방법
+/// statusData[StatusType.HP].statusValue -= 10; //타입을 key로 접근하시고, statusValue를 수정해주시면 됩니다!
+/// StatusManager.Instance.SetDircy(); //이 함수를 호출하면 세이브됩니다! (아직 게임 강제종료 세이브는 구현 전입니다..!)
 /// </summary>
 /// 
     
@@ -31,7 +37,7 @@ public class GameManager : MonoSingleton<GameManager>
 
     //Data
     public SerializedDictionary<string, UpgradeData> upgradeData = new SerializedDictionary<string, UpgradeData>();
-    public SerializedDictionary<string, StatusData> statusData =new SerializedDictionary<string, StatusData>();
+    public SerializedDictionary<StatusType, StatusData> statusData =new SerializedDictionary<StatusType, StatusData>();
 
 
     private void OnEnable() {
